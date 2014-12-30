e40766bca454393550d80199cd66c8b3dc0c6cb1
RolandTa <mail@rolandtanner.ch>
2014-12-30 13:43:04 +0000

    first commit

---
+++ server/config/environment/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://0.0.0.0/vitaflix-test'
+  }
+};
